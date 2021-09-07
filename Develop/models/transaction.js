3c3e78216976715faa1780a8c5a2fb7d41add9ee
Cortland Styles-Brown <cstylesbrown@gmail.com>
2021-09-07 01:42:13 -0500

    first commit

---
+++ Develop/models/transaction.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const transactionSchema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: "What is the name of this transaction?"
+    },
+    value: {
+      type: Number,
+      required: "What is the amount of money?"
+    },
+    date: {
+      type: Date,
+      default: Date.now
+    }
+  }
+);
+
+const Transaction = mongoose.model("Transaction", transactionSchema);
+
+module.exports = Transaction;
